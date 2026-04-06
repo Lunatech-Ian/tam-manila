1b2cfa6579dfc0de82360da0cea3093cf9c9235c
Lunatech-Ian <febrekristian@gmail.com>
2026-04-06 05:28:34 -0400

    Create users.js

---
+++ data/users.js
@@ -0,0 +1,20 @@
+const USERS = {
+  user1: {
+    id: "user1",
+    name: "Test User 1",
+    tier: "basic",
+    segment: "new"
+  },
+  user2: {
+    id: "user2",
+    name: "Test User 2",
+    tier: "premium",
+    segment: "returning"
+  },
+  user3: {
+    id: "user3",
+    name: "Test User 3",
+    tier: "support",
+    segment: "issue"
+  }
+};
